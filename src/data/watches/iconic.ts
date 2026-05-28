01c108eceb1e4013bca34665ec66a5690035e6c8
Mahammad Nihal T U <156331518+Nihaaalu@users.noreply.github.com>
2026-05-28 23:36:38 +0530

    Update iconic.ts

--- src/data/watches/iconic.ts
+++ src/data/watches/iconic.ts
@@ -1,1 +1,20 @@
-
+export const watch = {
+  id: "tommy-hilfiger-iconic-1791538",
+  brand: "Tommy Hilfiger",
+  name: "Iconic Multi-Function Gold Tone Watch",
+  originalPrice: 17200,
+  discountedPrice: 9999,
+  category: "Analog",
+  stock: true,
+  featured: false,
+  description:
+    "From the Tommy Hilfiger Iconic collection, this striking 46mm timepiece features a brilliant gold ionic-plated stainless steel case and a matching link bracelet. The textured white dial offers an elegant look with gold-tone stick indexes, featuring a semi-circular day indicator at the top alongside dedicated date and 24-hour multifunction subdials.",
+  images: [
+    "/images/watches/TommyHilfiger/iconic/i1.jpeg",
+    "/images/watches/TommyHilfiger/iconic/i2.jpeg",
+    "/images/watches/TommyHilfiger/iconic/i3.jpeg",
+    "/images/watches/TommyHilfiger/iconic/i4.jpeg",
+    "/images/watches/TommyHilfiger/iconic/i5.jpeg",
+    "/images/watches/TommyHilfiger/iconic/i6.jpeg"
+  ]
+};
