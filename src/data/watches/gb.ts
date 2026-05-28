ea014b7d36d725e1c8373b973c7b0cefc64a914c
Mahammad Nihal T U <156331518+Nihaaalu@users.noreply.github.com>
2026-05-29 00:07:29 +0530

    Create gb.ts

---
+++ src/data/watches/gb.ts
@@ -0,0 +1,20 @@
+export const watch = {
+  id: "tommy-hilfiger-1710362",
+  brand: "Tommy Hilfiger",
+  name: "Quartz Analog Black Dial Gold Tone Watch",
+  originalPrice: 17200,
+  discountedPrice: 9999,
+  category: "Analog",
+  stock: true,
+  featured: false,
+  description:
+    "A sophisticated multifunction timepiece featuring a brilliant gold ionic-plated stainless steel case and link bracelet. The uniquely textured cross-hatch black dial features gold-tone stick indexes, bold Arabic numerals at the 12, 6, and 9 o'clock positions, a convenient date window, and three sub-dials for day, date, and 24-hour tracking.",
+  images: [
+    "/images/watches/TommyHilfiger/gb/gb1.jpeg",
+    "/images/watches/TommyHilfiger/gb/gb2.jpeg",
+    "/images/watches/TommyHilfiger/gb/gb3.jpeg",
+    "/images/watches/TommyHilfiger/gb/gb4.jpeg",
+    "/images/watches/TommyHilfiger/gb/gb5.jpeg",
+    "/images/watches/TommyHilfiger/gb/gb6.jpeg"
+  ]
+};
