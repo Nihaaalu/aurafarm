abb7e3cede83f8e48c438871a60783336311dbd5
Mahammad Nihal T U <156331518+Nihaaalu@users.noreply.github.com>
2026-05-28 16:42:44 +0530

    Create tommy-hilfiger-kyle.ts

---
+++ src/data/watches/tommy-hilfiger-kyle.ts
@@ -0,0 +1,17 @@
+export const watch = {
+  id: "tommy-hilfiger-kyle-1791695",
+  brand: "Tommy Hilfiger",
+  name: "Kyle Multi-Function Black Watch",
+  originalPrice: 17200,
+  discountedPrice: 9999,
+  category: "Analog",
+  stock: true,
+  featured: false,
+  description:
+    "Part of the Tommy Hilfiger Kyle collection, this 44mm timepiece features a stealthy black ionic-plated stainless steel case and a matching link bracelet. The sleek black dial features multi-function subdials displaying dual time zones (Dual HR and Dual MIN) alongside a unique date window sitting perfectly at the 12 o'clock position.",
+  images: [
+    "/images/watches/TommyHilfiger/tommy-hilfiger-kyle/kyl1.jpeg",
+    "/images/watches/TommyHilfiger/tommy-hilfiger-kyle/kyl2.jpeg",
+    "/images/watches/TommyHilfiger/tommy-hilfiger-kyle/kyl3.jpeg"
+  ]
+};
