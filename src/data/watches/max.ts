6fe847e383cd8a1997f0b12df29aecf8143206ac
Mahammad Nihal T U <156331518+Nihaaalu@users.noreply.github.com>
2026-05-28 23:47:09 +0530

    Create max.ts

---
+++ src/data/watches/max.ts
@@ -0,0 +1,21 @@
+export const watch = {
+  id: "tommy-hilfiger-max-1791974",
+  brand: "Tommy Hilfiger",
+  name: "Max Multi-Function Black Dial Gold Watch",
+  originalPrice: 17200,
+  discountedPrice: 9999,
+  category: "Analog",
+  stock: true,
+  featured: false,
+  description:
+    "From the Tommy Hilfiger Max collection, this premium 44mm timepiece features a polished gold ionic-plated stainless steel case and a matching link bracelet. The textured black dial creates a powerful contrast, accented by gold-tone hands, stick indexes, a bold Arabic 12, and a tachymeter bezel. It includes three multifunction subdials tracking day, date, and 24-hour timekeeping.",
+  images: [
+    "/images/watches/TommyHilfiger/max/m1.jpeg",
+    "/images/watches/TommyHilfiger/max/m2.jpeg",
+    "/images/watches/TommyHilfiger/max/m3.jpeg",
+    "/images/watches/TommyHilfiger/max/m4.jpeg",
+    "/images/watches/TommyHilfiger/max/m5.jpeg",
+    "/images/watches/TommyHilfiger/max/m6.jpeg",
+    "/images/watches/TommyHilfiger/max/m7.jpeg"
+  ]
+};
